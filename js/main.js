5cd044085beaa31d6a7e9858314792ed09bcaad4
Humoyun2003 <abduqodirovh595>
2021-09-11 13:07:30 +0300

    End of mian js

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const elCard1 = document.querySelector('.myDiv1');
+const elCardOpenBtn1 = document.querySelector('.btn1');
+
+elCardOpenBtn1.addEventListener('click', () => {
+	elCard1.classList.toggle('myStyle');
+});
+const elCard2 = document.querySelector('.myDiv2');
+const elCardOpenBtn2 = document.querySelector('.btn2');
+
+elCardOpenBtn2.addEventListener('click', () => {
+	elCard2.classList.toggle('myStyle');
+});
+const elCard3 = document.querySelector('.myDiv3');
+const elCardOpenBtn3 = document.querySelector('.btn3');
+
+elCardOpenBtn3.addEventListener('click', () => {
+	elCard3.classList.toggle('myStyle');
+});
+const elCard4 = document.querySelector('.myDiv4');
+const elCardOpenBtn4 = document.querySelector('.btn4');
+
+elCardOpenBtn4.addEventListener('click', () => {
+	elCard4.classList.toggle('myStyle');
+});
+const elCard5 = document.querySelector('.myDiv5');
+const elCardOpenBtn5 = document.querySelector('.btn5');
+
+elCardOpenBtn5.addEventListener('click', () => {
+	elCard5.classList.toggle('myStyle');
+});
+const elCard6 = document.querySelector('.myDiv6');
+const elCardOpenBtn6 = document.querySelector('.btn6');
+
+elCardOpenBtn6.addEventListener('click', () => {
+	elCard6.classList.toggle('myStyle');
+});
+const elCard7 = document.querySelector('.myDiv7');
+const elCardOpenBtn7 = document.querySelector('.btn7');
+
+elCardOpenBtn7.addEventListener('click', () => {
+	elCard7.classList.toggle('myStyle');
+});
+const elCard8 = document.querySelector('.myDiv8');
+const elCardOpenBtn8 = document.querySelector('.btn8');
+
+elCardOpenBtn8.addEventListener('click', () => {
+	elCard8.classList.toggle('myStyle');
+});
